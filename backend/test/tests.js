dd8bdd69125a62a18ab5f375754cebbdedf60161
bovck <guvianacarvalho@hotmail.com>
2026-05-19 11:54:11 -0300

    voltando ao estado original

--- backend/test/tests.js
+++ backend/test/tests.js
@@ -224,8 +224,7 @@ describe("controllers/fatura", () => {
     expect(findQuery).to.deep.equal({ criador: "user-1" });
     expect(sortQuery).to.deep.equal({ createdAt: -1 });
     expect(res.statusCode).to.equal(200);
-    // expect(res.body.faturas).to.deep.equal(faturas);
-    expect(res.body.faturas).to.not.equal(faturas);
+    expect(res.body.faturas).to.deep.equal(faturas);
     expect(next.calls).to.be.empty;
   });
 
